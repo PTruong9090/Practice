9644fdfd87c99782a16a27e86ec111c70fdc7489
Phuc Truong <ptruong9090@gmail.com>
2026-05-11 16:35:53 -0700

    created api calls to backend

---
+++ notes-app/frontend/src/api/notesApi.js
@@ -0,0 +1,22 @@
+import { request } from "./request"
+
+
+export function getNotes() {
+    return request('/notes', {
+        method: 'GET',
+    })
+}
+
+export function createNote(note) {
+    return request('/notes', {
+        method: 'POST',
+        body: JSON.stringify(note),
+    })
+}
+
+export function updateNote(id, note) {
+    return request(`/notes/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify(note),
+    })
+}
