7f33a4d772a8c3c5c880b6ed9571fbb8408d963a
Phuc Truong <ptruong9090@gmail.com>
2026-05-11 17:51:04 -0700

    fixed api requests

--- notes-app/frontend/src/api/notesApi.js
+++ notes-app/frontend/src/api/notesApi.js
@@ -2,20 +2,20 @@ import { request } from "./request"
 
 
 export function getNotes() {
-    return request('/notes', {
+    return request('notes', {
         method: 'GET',
     })
 }
 
 export function createNote(note) {
-    return request('/notes', {
+    return request('notes', {
         method: 'POST',
         body: JSON.stringify(note),
     })
 }
 
 export function updateNote(id, note) {
-    return request(`/notes/${id}`, {
+    return request(`notes/${id}`, {
         method: 'PUT',
         body: JSON.stringify(note),
     })
